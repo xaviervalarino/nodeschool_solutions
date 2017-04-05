9106e30174c081d7bb45c0da90eaadbad73e5f3f
xaviervalarino <xavier@valarino.com>
2017-04-04 22:49:45 -0700

    Clean up duplexer-redux exercise

--- stream-adventure/12-duplexer-redux.js
+++ stream-adventure/12-duplexer-redux.js
@@ -1,31 +1,3 @@
-//In this example, you will be given a readable stream, `counter`, as the first
-//argument to your exported function:
-//
-//    module.exports = function (counter) {
-//        // return a duplex stream to count countries on the writable side
-//        // and pass through `counter` on the readable side
-//    };
-//
-//Return a duplex stream with the `counter` as the readable side. You will be
-//written objects with a 2-character `country` field as input, such as these:
-//
-//    {"short":"OH","name":"Ohio","country":"US"}
-//    {"name":"West Lothian","country":"GB","region":"Scotland"}
-//    {"short":"NSW","name":"New South Wales","country":"AU"}
-//
-//Create an object to track the number of occurrences of each unique country code.
-//
-//For example:
-//
-//    {"US": 2, "GB": 3, "CN": 1}
-//
-//Once the input ends, call `counter.setCounts()` with your counts object.
-//
-//The `duplexer2` module will again be very handy in this example.
-//
-//If you use duplexer, make sure to `npm install duplexer2` in the directory where
-//your solution file is located.
-var stream = require('stream')
 var duplexer = require('duplexer2');
 var through = require('through2').obj;
 
